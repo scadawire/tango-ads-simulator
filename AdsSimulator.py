e00e7f5cba604a505f7bdeba65f7dbb29f021cf5
Sebastian Jennen <sj@imagearts.de>
2025-04-15 13:55:37 +0200

    initial version

---
+++ AdsSimulator.py
@@ -0,0 +1,44 @@
+# see also: https://pypi.org/project/pyads/
+#
+import time
+from tango import AttrQuality, AttrWriteType, DispLevel, DevState, Attr, CmdArgType, UserDefaultAttrProp, Util
+from tango.server import Device, attribute, command, DeviceMeta
+from tango.server import class_property, device_property
+from tango.server import run
+import os
+import json
+from threading import Thread
+from threading import Lock
+import datetime
+import pyads
+import re
+from json import JSONDecodeError
+
+class AdsSimulator(Device, metaclass=DeviceMeta):
+    pass
+    host = device_property(dtype=str, default_value="127.0.0.1")
+    port = device_property(dtype=int, default_value=48898)
+    simulator_server = 0
+
+    @attribute
+    def time(self):
+        return time.time()
+
+    def init_device(self):
+        self.set_state(DevState.INIT)
+        self.get_device_properties(self.get_device_class())
+        h = AdvancedHandler()
+        h.add_variable(PLCVariable("Main.double",bytes(8),ads_type=constants.ADST_REAL64,symbol_type="LREAL"))
+        h.add_variable(PLCVariable("Main.float",bytes(4),ads_type=constants.ADST_REAL32,symbol_type="REAL"))
+        h.add_variable(PLCVariable("Main.long",bytes(8),ads_type=constants.ADST_INT64,symbol_type="INT"))
+        h.add_variable(PLCVariable("Main.bool",bytes(1),ads_type=constants.ADST_BIT,symbol_type="INT"))
+        h.add_variable(PLCVariable("Main.string",bytes(1024),ads_type=constants.ADST_STRING,symbol_type="STRING"))
+        self.simulator_server = AdsTestServer(handler=handler, logging=False, ip_address=self.host, port=self.port)
+        self.simulator_server.start()
+        print("test server started on " + self.address + ":" + str(self.port) + "...")
+        self.simulator_server.join()
+        self.set_state(DevState.ON)
+
+if __name__ == "__main__":
+    deviceServerName = os.getenv("DEVICE_SERVER_NAME")
+    run({deviceServerName: AdsSimulator})
---
+++ README.md
@@ -0,0 +1,15 @@
+# tango-ads-simulator
+
+scadawire/tango-controls simulation of a Bechhoff Ads device.
+
+# Structure
+
+The integration makes use of the tango python binding to implement device driver functionality.
+See also https://tango-controls.readthedocs.io/en/latest/development/device-api/python/index.html
+
+The ads specific functionality is covered by the pyads package.
+See also https://pyads.readthedocs.io/en/latest/index.html
+
+# Requirements
+
+unknown
